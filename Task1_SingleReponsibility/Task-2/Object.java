84347f2cf82713590d5eb28ca8567cf68fdd9fb6
Nafizanowrin <nafizaislamnowrin2gmaul.com>
2023-07-16 01:01:15 +0600

    message

---
+++ Task1_SingleReponsibility/Task-2/Object.java
@@ -0,0 +1,9 @@
+//Object creation
+public class Object{ 
+    String s = "Hello World"; 
+
+    public static void main(String args[]) { 
+    Object a = new Object(); //creating an object
+    System.out.println(a.s); //Outputs Hello World
+    } 
+}
---
+++ Task1_SingleReponsibility/Task-2/Variable.java
@@ -0,0 +1,12 @@
+//Variable declaration
+class Variable
+{
+        public static void main (String args[])
+        {
+            int student_id = 10; 
+            System.out.println(student_id);
+
+        }
+}
+
+//Output:10
