247f3fd7bd442daefae63c38c1d9da602de3f4dd
Nafizanowrin <115865205+Nafizanowrin@users.noreply.github.com>
2023-07-17 10:46:20 +0600

    Delete Task1_SingleReponsibility/Task-2 directory

--- Task1_SingleReponsibility/Task-2/Object.java
+++
@@ -1,9 +0,0 @@
-//Object creation
-public class Object{ 
-    String s = "Hello World"; 
-
-    public static void main(String args[]) { 
-    Object a = new Object(); //creating an object
-    System.out.println(a.s); //Outputs Hello World
-    } 
-}
